8dbc430a3395e6708f41e6ba256034152ba4e856
Shi Keming <1269736459@qq.com>
2025-01-20 16:03:48 +0800

    PA2 all inst

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -82,7 +82,7 @@ static void execute(uint64_t n) {
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);
-    if (nemu_state.state != NEMU_RUNNING) break;
+    if (nemu_state.state != NEMU_RUNNING) break;//stoppoint在此处触发break跳出for循环
     IFDEF(CONFIG_DEVICE, device_update());
   }
 }
