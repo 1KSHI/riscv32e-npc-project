a6306eead8aef30b547621fd09877f4dcb182313
Shi Keming <1269736459@qq.com>
2025-02-01 21:55:25 +0800

    PA2.1 npc add ebreak

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -6,27 +6,29 @@ TESTBENCH<Vysyx_24110026_top> *__TB__;
 //R-type
 //000000000010 00001 000 00000 0010011
 
-uint32_t mem[64]={0x00208093,0x00308093,0x00408093,0x00508093};
+uint32_t mem[64]={0x00208093,0x00308093,0x00408093,0x00508093,0x00308093,0x00408093,0x00508093,0x00100073};
 
 static uint32_t pmem_read(uint32_t addr) {
   return mem[addr];
+}
+
+extern "C" void ebreak() {
+  TB(~TESTBENCH());
+  exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char *argv[]) {
     __TB__ = new TESTBENCH<Vysyx_24110026_top>(argc, argv);
     TB(sim_init());
     TB(sim_reset());
-    TB(DUT(inst) = pmem_read((TB(DUT(pc))-0x80000000)/4));
-    TB(cycles(1));
-    TB(DUT(inst) = pmem_read((TB(DUT(pc))-0x80000000)/4));
-    TB(cycles(1));
-    TB(DUT(inst) = pmem_read((TB(DUT(pc))-0x80000000)/4));
-    TB(cycles(1));
-    TB(DUT(inst) = pmem_read((TB(DUT(pc))-0x80000000)/4));
+    for(int i=0;i<10;i++){
+        TB(DUT(inst) = pmem_read((TB(DUT(pc))-0x80000000)/4));
+        TB(cycles(1));
+    }
     TB(cycles(10));
 
-    TB(~TESTBENCH());
-    exit(EXIT_SUCCESS);
+    
+    //exit(EXIT_SUCCESS);
 }
 
 // VerilatedContext* contextp = NULL;
--- npc/vsrc/id.v
+++ npc/vsrc/id.v
@@ -1,5 +1,5 @@
 `include "./vsrc/defines.v"
-
+import "DPI-C" function void ebreak();
 
 module ysyx_24110026_decoder(
     input clk,
@@ -57,37 +57,38 @@ end
 assign branch_offset=inst_B_type?imm_b:32'b0;
 assign jump_offset=inst_J_type?imm_j:32'b0;
 
-wire inst_lui   = opcode[6:2]==`LUI_OP;//01101
-wire inst_auipc = opcode[6:2]==`AUIPC_OP;//00101
-wire inst_jal   = opcode[6:2]==`JAL_OP;//11011
-wire inst_jalr  = opcode[6:2]==`JALR_OP;//11001
-wire inst_load   = (opcode[6:2]==`LOAD_OP)&&(opcode[1:0]==2'b11);//00000
-wire inst_ebreak = opcode[6:2]==`EBREAK_OP;//11100
-wire inst_fence  = opcode[6:2]==`FENCE_OP;//00011
+wire inst_ebreak = inst==`EBREAK;
 
+wire inst_lui    = opcode[6:0]==`LUI_OP;//0110111
+wire inst_auipc  = opcode[6:0]==`AUIPC_OP;//0010111
+wire inst_jal    = opcode[6:0]==`JAL_OP;//1101111
+wire inst_jalr   = opcode[6:0]==`JALR_OP;//1100111
+wire inst_load   = opcode[6:0]==`LOAD_OP;//0000011
+// wire inst_F_type = opcode[6:0]==`FENCE_OP;//0001111
 wire inst_J_type = inst_jal;
 wire inst_U_type = inst_lui | inst_auipc;
-wire inst_R_type = opcode[6:2]==`R_TYPE_OP;//01100
-wire inst_B_type = opcode[6:2]==`B_TYPE_OP;//11000
-wire inst_S_type = opcode[6:2]==`S_TYPE_OP;//01000
-wire inst_I_type_base = opcode[6:2]==`I_TYPE_OP;//00100
+wire inst_R_type = opcode[6:0]==`R_TYPE_OP;//0110011
+wire inst_B_type = opcode[6:0]==`B_TYPE_OP;//1100011
+wire inst_S_type = opcode[6:0]==`S_TYPE_OP;//0100011
+wire inst_I_type_base = opcode[6:0]==`I_TYPE_OP;//0010011
+wire inst_I_type = inst_I_type_base | inst_load | inst_jalr;
 
-wire inst_I_type = inst_I_type_base | inst_load | inst_ebreak | inst_fence | inst_jalr;
+
 //add
-wire inst_add = inst_R_type && funct3 == 3'b000 && funct7 == 7'b0000000;
+wire inst_add  = inst_R_type      && funct3 == 3'b000 && funct7 == 7'b0000000;
 wire inst_addi = inst_I_type_base && funct3 == 3'b000;
 
-wire inst_lb = inst_load && funct3 == 3'b000;
-wire inst_lh = inst_load && funct3 == 3'b001;
-wire inst_lw = inst_load && funct3 == 3'b010;
+wire inst_lb  = inst_load && funct3 == 3'b000;
+wire inst_lh  = inst_load && funct3 == 3'b001;
+wire inst_lw  = inst_load && funct3 == 3'b010;
 wire inst_lbu = inst_load && funct3 == 3'b100;
 wire inst_lhu = inst_load && funct3 == 3'b101;
 wire inst_lwu = inst_load && funct3 == 3'b110;
 
-wire inst_beq = inst_B_type && funct3 == 3'b000;
-wire inst_bne = inst_B_type && funct3 == 3'b001;
-wire inst_blt = inst_B_type && funct3 == 3'b100;
-wire inst_bge = inst_B_type && funct3 == 3'b101;
+wire inst_beq  = inst_B_type && funct3 == 3'b000;
+wire inst_bne  = inst_B_type && funct3 == 3'b001;
+wire inst_blt  = inst_B_type && funct3 == 3'b100;
+wire inst_bge  = inst_B_type && funct3 == 3'b101;
 wire inst_bltu = inst_B_type && funct3 == 3'b110;
 wire inst_bgeu = inst_B_type && funct3 == 3'b111;
 
@@ -96,29 +97,29 @@ wire inst_sh = inst_S_type && funct3 == 3'b001;
 wire inst_sw = inst_S_type && funct3 == 3'b010;
 
 //sub 
-wire inst_sub = inst_R_type && funct3 == 3'b000 && funct7 == 7'b0100000;
-wire inst_slti = inst_I_type_base && funct3 == 3'b010;
+wire inst_sub   = inst_R_type      && funct3 == 3'b000 && funct7 == 7'b0100000;
+wire inst_slti  = inst_I_type_base && funct3 == 3'b010;
 wire inst_sltiu = inst_I_type_base && funct3 == 3'b011;
-wire inst_slt = inst_R_type && funct3 == 3'b010 && funct7 == 7'b0000000;
-wire inst_sltu = inst_R_type && funct3 == 3'b011 && funct7 == 7'b0000000;
+wire inst_slt   = inst_R_type      && funct3 == 3'b010 && funct7 == 7'b0000000;
+wire inst_sltu  = inst_R_type      && funct3 == 3'b011 && funct7 == 7'b0000000;
 
 //xor funct3:100
-wire inst_xor = inst_R_type && funct3 == 3'b100 && funct7 == 7'b0000000;
+wire inst_xor  = inst_R_type      && funct3 == 3'b100 && funct7 == 7'b0000000;
 wire inst_xori = inst_I_type_base && funct3 == 3'b100;
 //or funct3:110
-wire inst_or = inst_R_type && funct3 == 3'b110 && funct7 == 7'b0000000;
-wire inst_ori = inst_I_type_base && funct3 == 3'b110;
+wire inst_or   = inst_R_type      && funct3 == 3'b110 && funct7 == 7'b0000000;
+wire inst_ori  = inst_I_type_base && funct3 == 3'b110;
 //and funct3:111
-wire inst_and = inst_R_type && funct3 == 3'b111 && funct7 == 7'b0000000;
+wire inst_and  = inst_R_type      && funct3 == 3'b111 && funct7 == 7'b0000000;
 wire inst_andi = inst_I_type_base && funct3 == 3'b111;
 //srl funct3:101 funct7:0000000
-wire inst_srl = inst_R_type && funct3 == 3'b101 && funct7 == 7'b0000000;
+wire inst_srl  = inst_R_type      && funct3 == 3'b101 && funct7 == 7'b0000000;
 wire inst_srli = inst_I_type_base && funct3 == 3'b101;
 //sll funct3:001
-wire inst_sll = inst_R_type && funct3 == 3'b001 && funct7 == 7'b0000000;
+wire inst_sll  = inst_R_type      && funct3 == 3'b001 && funct7 == 7'b0000000;
 wire inst_slli = inst_I_type_base && funct3 == 3'b001;
 //sra funct3:101 funct7:0100000
-wire inst_sra = inst_R_type && funct3 == 3'b101 && funct7 == 7'b0100000;
+wire inst_sra  = inst_R_type      && funct3 == 3'b101 && funct7 == 7'b0100000;
 wire inst_srai = inst_I_type_base && funct3 == 3'b101;
 //mul
 
@@ -127,9 +128,9 @@ wire inst_srai = inst_I_type_base && funct3 == 3'b101;
 //rem
 //alu_add 001
 assign alu_op[0] =  inst_add | inst_addi | inst_auipc | inst_jalr | inst_jal | 
-                inst_lb | inst_lh | inst_lw | inst_lbu | inst_lhu | inst_lwu |
-                inst_sb | inst_sh | inst_sw | inst_beq | inst_bne | inst_blt | 
-                inst_bge | inst_bltu | inst_bgeu;
+                    inst_lb  | inst_lh   | inst_lw    | inst_lbu  | inst_lhu | 
+                    inst_lwu | inst_sb   | inst_sh    | inst_sw   | inst_beq | 
+                    inst_bne | inst_blt  | inst_bge   | inst_bltu | inst_bgeu;
 //alu_sub 010
 assign alu_op[1] =  inst_sub | inst_slti | inst_sltiu | inst_slt | inst_sltu;
 //alu_xor 011
@@ -145,6 +146,11 @@ assign alu_op[6] =  inst_sll | inst_slli;
 //alu_sra 1000
 assign alu_op[7] =  inst_sra | inst_srai;
 
+always@(clk)begin
+    if(inst_ebreak)begin
+        ebreak();
+    end
+end
 
 endmodule
 
