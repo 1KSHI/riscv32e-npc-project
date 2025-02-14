d8b310500b2f7e3bb879f7f8430f78d420cd2c89
Shi Keming <1269736459@qq.com>
2025-02-14 19:48:47 +0800

    PA2.2 am string

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -9,12 +9,73 @@ int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
 
+//将整数转换为字符串
+static void itoa(int n, char *str) {
+  char temp[20];
+  int i = 0, j = 0;
+  if (n == 0) {
+    str[i++] = '0';
+  } else {
+    if (n < 0) {
+      str[i++] = '-';
+      n = -n;
+    }
+    while (n > 0) {
+      temp[j++] = (n % 10) + '0';
+      n /= 10;
+    }
+    while (j > 0) {
+      str[i++] = temp[--j];
+    }
+  }
+  str[i] = '\0';
+}
+
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  const char *p = fmt;
+  char *s;
+  int d;
+  char buffer[20];
+  char *out_ptr = out;
+
+  while (*p) {
+    if (*p == '%') {
+      p++;
+      switch (*p) {
+        case 's':
+          s = va_arg(ap, char *);
+          while (*s) {
+            *out_ptr++ = *s++;
+          }
+          break;
+        case 'd':
+          d = va_arg(ap, int);
+          itoa(d, buffer);
+          s = buffer;
+          while (*s) {
+            *out_ptr++ = *s++;
+          }
+          break;
+        default:
+          *out_ptr++ = '%';
+          *out_ptr++ = *p;
+          break;
+      }
+    } else {
+      *out_ptr++ = *p;
+    }
+    p++;
+  }
+  *out_ptr = '\0';
+  return out_ptr - out;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int result = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return result;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- nemu/src/utils/disasm.c
+++ nemu/src/utils/disasm.c
@@ -60,6 +60,7 @@ void init_disasm() {
 }
 
 void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
+  // printf("disassemble pc = 0x%lx\n", pc);
 	cs_insn *insn;
 	size_t count = cs_disasm_dl(handle, code, nbyte, pc, 0, &insn);
   assert(count == 1);
